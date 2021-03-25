1748863d3926fa6c0b7f7c023b3259a695552f9c
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-03-26 06:47:37 +0700

    add register route

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from market.models import Item
 from flask import render_template
+from market.forms import RegisterForm
 
 @app.route('/')
 @app.route('/home')
@@ -16,3 +17,8 @@ def market_page():
 	# ]
 	items = Item.query.all()
 	return render_template('market.html', items=items)
+
+@app.route('/register')
+def register_page():
+	form = RegisterForm()
+	return render_template('register.html', form=form)
