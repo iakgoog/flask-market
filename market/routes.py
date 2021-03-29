f0e32989b6bdd79067d4d6940d33a6e5384024f4
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-03-30 06:41:30 +0700

    validate form on submit

--- market/routes.py
+++ market/routes.py
@@ -1,25 +1,34 @@
-from market import app
-from market.models import Item
-from flask import render_template
+from market import app, db
+from market.models import Item, User
+from flask import render_template, redirect, url_for
 from market.forms import RegisterForm
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-	return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/market')
 def market_page():
-	# example data
-	# items = [
-	# 	{'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-	# 	{'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-	# 	{'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-	# ]
-	items = Item.query.all()
-	return render_template('market.html', items=items)
+    # example data
+    # items = [
+    # 	{'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+    # 	{'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+    # 	{'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    # ]
+    items = Item.query.all()
+    return render_template('market.html', items=items)
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
-	form = RegisterForm()
-	return render_template('register.html', form=form)
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    return render_template('register.html', form=form)
