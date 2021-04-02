cdb06f425162f52238d05c7f303d76dc12e2b0ac
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-04-02 18:02:17 +0700

    add missing import (ValidationError)

--- market/forms.py
+++ market/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from market.models import User
 
 class RegisterForm(FlaskForm):
